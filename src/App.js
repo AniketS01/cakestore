a6784b0f09a05df7abf06504659404e8bb597e2a
ulhas bhalerao <bhaleraoulhas01@gmail.com>
2021-10-20 13:42:07 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import img from "./imgs/img1.svg";
+import "./App.css";
+const App = () => {
+  const [cakeImg, setCakeImg] = useState();
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="row maindiv">
+        <h1 className="text-center mb-5">Add a New Cake</h1>
+        {/* leftside Image */}
+        <div className="col-lg-6">
+          <img src={img} className="img-fluid" alt="" />
+        </div>
+        {/* The product div */}
+        <div className=" col-lg-6  d-flex flex-column">
+          <label htmlFor="" style={{ fontWeight: "600" }}>
+            {" "}
+            Product Image :
+            <input
+              type="file"
+              onChange={(e) => setCakeImg(e.target.files[0])}
+              className="mb-2"
+            />
+            {cakeImg && (
+              <img
+                style={{ height: "80px", width: "80px" }}
+                alt="enter img"
+                src={URL.createObjectURL(cakeImg)}
+              />
+            )}
+          </label>
+          <label htmlFor="" style={{ fontWeight: "600" }}>
+            Product Name :{" "}
+            <input
+              type="text"
+              placeholder="Enter Product name"
+              className="mb-2"
+            />
+          </label>
+          <label htmlFor="" style={{ fontWeight: "600" }}>
+            Product description :{" "}
+          </label>
+          <textarea
+            name=""
+            id=""
+            cols="15"
+            rows="5"
+            placeholder="Enter a Description "
+            className="mb-2"
+          ></textarea>
+          <label htmlFor="" style={{ fontWeight: "600" }}>
+            Product Price :{" "}
+            <input type="text" placeholder="Enter price" className="mb-2" />
+          </label>
+
+          <label htmlFor="" style={{ fontWeight: "600" }}>
+            Category :{" "}
+            <select name="" id="" className="mb-2">
+              <option value="">Designer cake</option>
+              <option value="">Festival cake</option>
+              <option value="">bla bla cake</option>
+            </select>
+          </label>
+          <label htmlFor="" style={{ fontWeight: "600" }}>
+            Offer or Coupons :{" "}
+            <input type="text" placeholder="Enter offers or coupon" />
+          </label>
+
+          <button className="btn btn-md btn-primary mt-3 ">Submit</button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
